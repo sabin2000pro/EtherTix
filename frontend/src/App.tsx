e3d41d442898852d2f6aecfa71cdaa3aea268f82
sabin2000pro <sabinlungu293@gmail.com>
2022-09-27 12:48:50 +0100

    test

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+       <h1>EtherTix</h1>
+    </div>
+  );
+}
+
+export default App;
