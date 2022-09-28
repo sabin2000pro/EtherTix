44a2c459c4da056079590e4113b3329eb5f55880
sabin2000pro <sabinlungu293@gmail.com>
2022-09-28 14:26:02 +0100

    Writing docker compose file

--- backend/services/authentication/src/controllers/auth-controller.ts
+++ backend/services/authentication/src/controllers/auth-controller.ts
@@ -1,1 +1,10 @@
+import { NextFunction, Request, Response } from 'express';
 import User from '../models/user-model';
+
+export const registerUser = async (request: Request, response: Response, next: NextFunction): Promise<Response> => {
+    return response.status(200).json({message: "Register User", success: true});
+}
+
+export const loginUser = async (request: Request, response: Response, next: NextFunction): Promise<Response> => {
+    return response.status(200).json({message: "Login User", success: true});
+}
