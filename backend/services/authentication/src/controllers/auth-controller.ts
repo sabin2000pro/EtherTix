d59ea35da6a73aecceaf80ca3cf48d75a886cab3
sabin2000pro <sabinlungu293@gmail.com>
2022-09-28 08:51:32 +0100

    Adding basic template  for auth controllers

--- backend/services/authentication/src/controllers/auth-controller.ts
+++ backend/services/authentication/src/controllers/auth-controller.ts
@@ -1,1 +1,2 @@
 import User from '../models/user-model';
+
--- frontend/src/pages/auth/Login.tsx
+++ frontend/src/pages/auth/Login.tsx
@@ -2,11 +2,12 @@ import React, {useState, useEffect} from 'react'
 
 // @description: Login Component
 const Login: React.FC = (props) => {
+
   return (
     <>
    
     <div>
-
+       Login Page
     </div>
 
     </>
