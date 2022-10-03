30e0e91498a280f9a8b9127c90b7e751d509c1a4
sabin2000pro <sabinlungu293@gmail.com>
2022-10-03 13:03:03 +0100

    Implementing the ticket model schema

--- backend/services/tickets/src/models/ticket-model.ts
+++ backend/services/tickets/src/models/ticket-model.ts
@@ -18,9 +18,38 @@ const TicketSchema = new mongoose.Schema<ITicketDocument>({ // Ticket Data Schem
             type: String
         },
 
-        capacity: { // Number of tickets for sale
-            type: String;
+        capacity: { // Number of tickets for sale (0, 1, 2,3)
+            type: String,
+            required: [true, "Please specify the number of tickets available for sale"],
+            default: null
+        },
+
+        minimumQuantityPurchase: {
+
+        },
+
+        maximumQuantityPurchase: {
+
+        },
+
+        description: {
+            type: String,
+            required: [true, "Provide a valid description for the ticket"]
+        },
+
+        cost: { // The ticket cost in ETHER
+            type: String,
+            required: [true, "Please specify how much the ticket costs"],
+            default: "0.013"
+        },
+
+        isFree: {
+            type: Boolean,
+            required: [true, "Please specify if the ticket is free or not"],
+            default: false
         }
+
+
     }
 }, {
     timestamps: true
