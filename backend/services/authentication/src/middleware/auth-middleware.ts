606ffd61329390079ae2bf5d3b405b2c8150d3b7
sabin2000pro <sabinlungu293@gmail.com>
2022-10-10 21:11:27 +0100

    Added authentication middleware to check for valid JWT

--- backend/services/authentication/src/middleware/auth-middleware.ts
+++ backend/services/authentication/src/middleware/auth-middleware.ts
@@ -1,4 +1,7 @@
+import { BadRequestError, UnauthorizedError } from './error-handler';
 import { NextFunction, Request, Response } from "express";
+import { User } from '../models/user-model';
+import jwt from "jsonwebtoken";
 
 declare namespace Express {
     export interface Request {
@@ -12,19 +15,40 @@ declare namespace Express {
   }
   
 
-export const protectAuth = async (request: Request, response: Response, next: NextFunction): Promise<any> => {
+export const protectAuth = async (request: Express.Request, response: Response, next: NextFunction): Promise<any> => {
     let token;
 
-    if(!token) {
+    if(request.headers.authorization && request.headers.authorization.includes("Bearer ")) {
+         token = request.headers.authorization.split(' ')[1]; // Get the JWT token at the first index after Bearer
+    }
 
+    if(!token) {
+        return next(new UnauthorizedError("You are not authorized to perform this action"));
+    }
+
+    try {
+        const decoded: any = jwt.verify(token, process.env.JWT_SECRET);
+        request.user = await User.findById(decoded.id);
+
+        return next();
+    } 
+    
+    catch(error: any) {
+        if(error) {
+            return next(new UnauthorizedError("You are unauthorized to perform this action"));
+        }
     }
 
     
 }
 
 export const restrictRoleTo = (...roles: any) => {
-    
-    return (request: Request, response: Response, next: NextFunction) => {
-        
+
+    return (request: Express.Request, response: Response, next: NextFunction) => {
+
+        if(!roles.includes(request.user.role)) {
+
+        }
+
     }
 }
