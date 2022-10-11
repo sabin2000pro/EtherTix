5296c7a72fffdae31bf0cf97bef36ae8b6742c81
sabin2000pro <sabinlungu293@gmail.com>
2022-10-11 12:36:28 +0100

    Working on the authentication API

--- backend/services/authentication/src/utils/send-email.ts
+++ backend/services/authentication/src/utils/send-email.ts
@@ -7,8 +7,8 @@ return nodemailer.createTransport({
     host: "smtp.mailtrap.io",
     port: 2525,
     auth: {
-      user: "29c023d298c4ba",
-      pass: "7207ba67404426"
+      user: "97f91e0d2279c6",
+      pass: "44445d576ce426"
     }
   });
 
