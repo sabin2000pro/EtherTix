a0fc509d340f4e0eb7bd80c0f47f2fd18fe49c28
sabin2000pro <sabinlungu293@gmail.com>
2022-10-11 09:22:47 +0100

    Added geocoder middleware for the venues so the coordinates are shown

--- backend/services/venues/src/models/venue-model.ts
+++ backend/services/venues/src/models/venue-model.ts
@@ -26,6 +26,7 @@ const VenueSchema = new mongoose.Schema<IVenueDocument>({
   
         website: {
           type: String,
+          
           match: [
             /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
             'Please use a valid URL with HTTP or HTTPS'
