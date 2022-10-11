4d0dc83ee979d3ed803a327d4fbe0965f6c2b6b2
sabin2000pro <sabinlungu293@gmail.com>
2022-10-11 09:11:53 +0100

    Added geocoder middleware for the venues so the coordinates are shown

--- backend/services/venues/src/models/venue-model.ts
+++ backend/services/venues/src/models/venue-model.ts
@@ -21,6 +21,27 @@ const VenueSchema = new mongoose.Schema<IVenueDocument>({
             type: String,
             required: [true, "Please include a valid name for the name"]
         },
+  
+        website: {
+          type: String,
+          match: [
+            /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+            'Please use a valid URL with HTTP or HTTPS'
+          ]
+        },
+
+        phone: {
+          type: String,
+          maxlength: [20, 'Phone number can not be longer than 20 characters']
+        },
+
+        email: {
+          type: String,
+          match: [
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            'Please add a valid email'
+          ]
+        },
 
         slug: String,
 
@@ -34,6 +55,16 @@ const VenueSchema = new mongoose.Schema<IVenueDocument>({
             type: Number,
             required: [true, "Please specify the capacity of this venue"],
             default: 0
+        },
+
+        openTime: {
+          type: Date,
+          default: Date.now
+        },
+
+        closeTime: {  
+          type: Date,
+          default: Date.now
         },
 
         address: { // Address of the venue
