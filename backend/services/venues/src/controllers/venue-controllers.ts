23efe4f3033b2fb400de4b0a675be4cba25d8545
sabin2000pro <sabinlungu293@gmail.com>
2022-10-17 10:46:53 +0100

    Implementing basic controllers for the venues

--- backend/services/venues/src/controllers/venue-controllers.ts
+++ backend/services/venues/src/controllers/venue-controllers.ts
@@ -1,34 +1,82 @@
 import { Venue } from '../models/venue-model';
 import {Request, Response, NextFunction} from 'express';
 
+// @desc      Fetch All Event Tickets
+// @route     GET /api/v1/tickets
+// @route     GET /api/v1/events/:eventId/tickets
+// @access    Private (Authorization Token Required)
+
 export const fetchAllVenues = async (request: Request, response: Response, next: NextFunction): Promise<any> => {
 
 }
+
+// @desc      Fetch All Event Tickets
+// @route     GET /api/v1/tickets
+// @route     GET /api/v1/events/:eventId/tickets
+// @access    Private (Authorization Token Required)
 
 export const fetchVenueByID = async (request: Request, response: Response, next: NextFunction): Promise<any> => {
 
 }
 
+// @desc      Fetch All Event Tickets
+// @route     GET /api/v1/tickets
+// @route     GET /api/v1/events/:eventId/tickets
+// @access    Private (Authorization Token Required)
+
 export const createVenue = async (request: Request, response: Response, next: NextFunction): Promise<any> => {
 
 }
+
+// @desc      Fetch All Event Tickets
+// @route     GET /api/v1/tickets
+// @route     GET /api/v1/events/:eventId/tickets
+// @access    Private (Authorization Token Required)
 
 export const editVenueByID = async (request: Request, response: Response, next: NextFunction): Promise<any> => {
 
 }
 
+// @desc      Fetch All Event Tickets
+// @route     GET /api/v1/tickets
+// @route     GET /api/v1/events/:eventId/tickets
+// @access    Private (Authorization Token Required)
+
 export const deleteVenueByID = async (request: Request, response: Response, next: NextFunction): Promise<any> => {
 
 }
+
+// @desc      Fetch All Event Tickets
+// @route     GET /api/v1/tickets
+// @route     GET /api/v1/events/:eventId/tickets
+// @access    Private (Authorization Token Required)
 
 export const deleteAllVenues = async (request: Request, response: Response, next: NextFunction): Promise<any> => {
 
 }
 
+// @desc      Fetch All Event Tickets
+// @route     GET /api/v1/tickets
+// @route     GET /api/v1/events/:eventId/tickets
+// @access    Private (Authorization Token Required)
+
 export const fetchVenuesWithinRadius = async (request: Request, response: Response, next: NextFunction): Promise<any> => {
 
 }
 
+// @desc      Fetch All Event Tickets
+// @route     GET /api/v1/tickets
+// @route     GET /api/v1/events/:eventId/tickets
+// @access    Private (Authorization Token Required)
+
 export const uploadVenuePhoto = async (request: Request, response: Response, next: NextFunction): Promise<any> => {
+
+}
+
+// @desc      Fetch Trending Venues That Are Popular
+// @route     GET /api/v1/venues/trending
+// @access    Public (No Authorization Token Required)
+
+export const fetchTrendingVenues = async (request: Request, response: Response, next: NextFunction): Promise<any> => {
 
 }
