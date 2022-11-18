43ad46cab5ce00f7538d1ad51937f75210fd62ea
sabin2000pro <sabinlungu293@gmail.com>
2022-11-18 10:39:53 +0000

    Working on adding JOI validation middleware

--- backend/services/authentication/src/middleware/validation-middleware.ts
+++ backend/services/authentication/src/middleware/validation-middleware.ts
@@ -1,29 +1,27 @@
 import Joi from "joi";
 
-export const registerAccountSchema = Joi.object({
+export const registerUserValidationAgent = Joi.object({
     username: Joi.string().required(),
+    forename: Joi.string().required(),
     email: Joi.string().email().lowercase().required(),
     password: Joi.string().min(5).required(),
     role: Joi.string().required()
 });
 
 // @description: Login Validation
-export const loginSchema = Joi.object({
+export const loginValidationAgent = Joi.object({
     email: Joi.string().required(),
     password: Joi.string().required(),
 })
 
-export const forgotPasswordSchema = Joi.object({
+export const forgotPasswordValidationAgent = Joi.object({
     email: Joi.string().required()
 })
 
-export const verifyEmailAddressSchema = Joi.object({
-
+export const emailValidationAgent = Joi.object({
+    email: Joi.string().required()
 })
 
-export const loginMfaSchema = Joi.object({
-    
-})
 
 export const resetPasswordSchema = Joi.object({
 
