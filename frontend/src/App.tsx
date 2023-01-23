70d87c9a4880c7206f210fdd79b073504f3ac3db
sabin2000pro <sabinlungu293@gmail.com>
2023-01-23 16:20:55 +0000

    Fixed forgot password bug

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import ForgotPassword from 'pages/auth/ForgotPassword';
 import Login from 'pages/auth/Login';
 import Register from 'pages/auth/Register';
 import ResetPassword from 'pages/auth/ResetPassword';
+import UpdatePassword from 'pages/auth/UpdatePassword';
 import React from 'react';
 import {Routes, Route} from 'react-router-dom';
 
@@ -17,8 +18,10 @@ const App = () => { // Main App Component
             <Route path = '/register' element = {<Register />} />
             <Route path = '/login' element = {<Login />} />
             <Route path = '/forgot-password' element = {<ForgotPassword />} />
+
             <Route path = '/reset-password/:token' element = {<ResetPassword />} />
             <Route path = '/verify-email' element = {<EmailVerification />} />
+            <Route path = '/update-password' element = {<UpdatePassword />} />
 
          </Routes>
 
---
+++ frontend/src/pages/auth/UpdatePassword.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const UpdatePassword = () => {
+  return (
+
+    <>
+        Update Password Page
+    </>
+
+  )
+}
+
+export default UpdatePassword
