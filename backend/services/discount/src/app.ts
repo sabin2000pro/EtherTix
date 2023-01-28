42773a5383cbe1f9d0539e67c5e773ff30e9503d
sabin2000pro <sabinlungu293@gmail.com>
2023-01-28 23:30:50 +0000

    Pushing latest code changes

--- backend/services/discount/src/app.ts
+++ backend/services/discount/src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response } from "express";
+import express from 'express';
 import morgan from "morgan"
 import hpp from "hpp"
 import helmet from "helmet"
@@ -12,14 +12,19 @@ if(process.env.NODE_ENV === 'development') {
  
 app.use(express.json());
 app.set('trust proxy', true);
+
 app.use(hpp());
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ["POST", "GET", "PUT", "DELETE"]
+}));
+
 app.use(helmet());
 
 // Include error handling middleware here for the venues
 
 app.get("/", (request: any, response: any) => {
-    return response.json({message: "Discount Root Route"})
+    return response.json({message: "Discount Root Route Endpoint"})
 })
 
 export {app}
