475f755f5950d17c4e88c2d0f5bcb46f754539ca
sabin2000pro <sabinlungu293@gmail.com>
2023-01-29 11:21:18 +0000

    Updated makefile to build and push frontend service to docker hub script

--- backend/services/events/src/index.ts
+++ backend/services/events/src/index.ts
@@ -6,8 +6,16 @@ const port = process.env.PORT || 5301;
 
 export const startEventsServer = async () => {
 
-      return app.listen(port, () => {
-        console.log(`Event service live on port ${port} in mode : ${process.env.NODE_ENV}`);
+      return app.listen(port, (error) => {
+         if(!error) {
+          console.log(`Event service live on port ${port} in mode : ${process.env.NODE_ENV}`);
+         }
+
+         else {
+            return console.error(error);
+         }
+
+
       });
 
 }
