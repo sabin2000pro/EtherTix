dcafe691ae416fe6402551ed0d5f10e11f6bbe7b
sabin2000pro <sabinlungu293@gmail.com>
2023-01-29 10:59:58 +0000

    Updated makefile to build and push frontend service to docker hub script

--- backend/services/authentication/dist/database/auth-db.js
+++ backend/services/authentication/dist/database/auth-db.js
@@ -14,7 +14,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.connectAuthDatabase = void 0;
 const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config({ path: 'backend/services/authentication/config.env' });
+dotenv_1.default.config({ path: "backend/services/authentication/config.env" });
 const mongoose_1 = __importDefault(require("mongoose"));
 const AUTH_DB_URI = process.env.AUTH_DB_URI;
 const connectAuthDatabase = (...args) => __awaiter(void 0, void 0, void 0, function* () {
