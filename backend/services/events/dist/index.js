c4025800253dfc6ff4fddc9a09849439e3d5c920
sabin2000pro <sabinlungu293@gmail.com>
2023-01-29 11:20:49 +0000

    Updated makefile to build and push frontend service to docker hub script

--- backend/services/events/dist/index.js
+++ backend/services/events/dist/index.js
@@ -8,13 +8,18 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./app");
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config({ path: 'backend/services/events/config.env' });
 const port = process.env.PORT || 5301;
 // Start of authentication server
 const startAuthServer = () => __awaiter(void 0, void 0, void 0, function* () {
     return app_1.app.listen(port, () => {
-        console.log(`Event service live on port ${port}`);
+        console.log(`Event service live on port ${port} in mode : ${process.env.NODE_ENV}`);
     });
 });
 startAuthServer();
--- backend/services/events/src/index.ts
+++ backend/services/events/src/index.ts
@@ -4,8 +4,7 @@ dotenv.config({path: 'backend/services/events/config.env'})
 
 const port = process.env.PORT || 5301;
 
-// Start of authentication server
-const startAuthServer = async () => {
+export const startEventsServer = async () => {
 
       return app.listen(port, () => {
         console.log(`Event service live on port ${port} in mode : ${process.env.NODE_ENV}`);
@@ -13,4 +12,4 @@ const startAuthServer = async () => {
 
 }
 
-startAuthServer()
+startEventsServer();
