45489426b45d4469aaefd36868cf7a7315fb360a
sabin2000pro <sabinlungu293@gmail.com>
2023-01-29 11:19:04 +0000

    Updated makefile to build and push frontend service to docker hub script

--- backend/services/authentication/src/app.ts
+++ backend/services/authentication/src/app.ts
@@ -16,14 +16,9 @@ const app: any = express();
 
 connectAuthDatabase()
 
-if(process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev') as any);
-}
+app.use(morgan('dev') as any);
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(mongoSanitize()); // Prevent against NoSQL Injection attacks in production environment
-}
- 
+app.use(mongoSanitize()); // Prevent against NoSQL Injection attacks in production environment
 app.use(express.json());
 app.set('trust proxy', true);
 app.use(hpp());
