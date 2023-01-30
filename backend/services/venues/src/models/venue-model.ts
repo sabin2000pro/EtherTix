215c3aa144f498548507d2896193a6c6e9cec342
sabin2000pro <sabinlungu293@gmail.com>
2023-01-30 12:42:24 +0000

    Refactoring authentication service

--- backend/services/venues/src/models/venue-model.ts
+++ backend/services/venues/src/models/venue-model.ts
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 import geocoder from "node-geocoder";
 
 interface IVenueAttributes {
-    venue: Object;
     name: string;
     slug: string;
     email: string; // E-mail Address of the venue
@@ -59,18 +58,11 @@ const VenueSchema = new mongoose.Schema<IVenueDocument>({
             minlength: 6
         },
         
-        slug: { // Slug URL for the venue
-          type: String
-        },
+        slug: String,
   
-        website: {
+        website: { // Website URL of the venue
           type: String,
-
-          match: [
-            /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
-            'Please use a valid URL with HTTP or HTTPS'
-          ]
-
+          match: [/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/, 'Please use a valid URL with HTTP or HTTPS']
         },
 
         phone: { // Phone number for the venue
@@ -80,13 +72,9 @@ const VenueSchema = new mongoose.Schema<IVenueDocument>({
         },
 
         email: { // Contact e-mail address for the venue
-
           type: String,
-
-          match: [
-            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-            'Please add a valid email'
-          ]
+          match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please add a valid e-mail address for the venue for contact purposes'],
+          required: [true, "Please specify the e-mail address for the Venue"]
         },
 
         ageRestriction: { // Venue entry age restriction
@@ -103,12 +91,14 @@ const VenueSchema = new mongoose.Schema<IVenueDocument>({
 
         openTime: { // The opening time of the venue
            type: Date,
-           default: Date.now
+           default: Date.now,
+           required: [true, "Please specify the date at which the Venue Opens"]
         },
 
         closeTime: {  // The current time at which the venue shuts
            type: Date,
-           default: Date.now
+           default: Date.now,
+           required: [true, "Please specify the date at which the shuts"]
         },
 
         hasPublicAccess: { // Field that identifies if the venue allows public access or not
@@ -160,13 +150,15 @@ const VenueSchema = new mongoose.Schema<IVenueDocument>({
 
         organiser: { // Event Venue Organiser: One venue at a time can have only one organiser
             type: mongoose.Schema.Types.ObjectId,
-            ref: "User"
+            ref: "User",
+            required: [true, "Please specify the Organiser ID at this Venue"]
         },
 
-        event: [{ // One venue can have multiple events being hosted at different dates
+        event: { // One venue can have multiple events being hosted at different dates
           type: mongoose.Schema.Types.ObjectId,
-          ref: "Event"
-        }],
+          ref: "Event",
+          required: [true, "Please specify a valid Event ID which is being hosted at this event at a given time"]
+        },
 
         ticket: [{ // One venue can have many tickets available associated to it
           type: mongoose.Schema.Types.ObjectId,
