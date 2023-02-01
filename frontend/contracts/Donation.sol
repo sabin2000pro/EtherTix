03ddd64785399b4ec2a5271eae5275cc34e4b38c
sabin2000pro <sabinlungu293@gmail.com>
2023-02-01 13:44:59 +0000

    Added test files in the frontend for venues service

---
+++ frontend/contracts/Donation.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+
+contract Donation is ERC721URIStorage, Ownable {
+   
+    constructor() ERC721("Donation Contract", "DNCT") {
+
+    }
+    
+}
