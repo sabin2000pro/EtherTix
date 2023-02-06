c1c7481b8b088199862461c24f5346571a5af410
sabin2000pro <sabinlungu293@gmail.com>
2023-02-06 13:24:53 +0000

    Pushing to master

---
+++ frontend/test/TicketNFT.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-undef
+const TicketNFT = artifacts.require("TicketNFT")
+
+contract("TicketNFT", accounts => {
+
+    describe("Ticket NFT Test suite", () => {
+
+        
+
+       beforeEach(async () => {
+          console.log(`Inside before each`)
+       })
+
+    })
+})
