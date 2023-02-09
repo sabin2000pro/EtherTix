ee583d051e8d44153c8bc45e3c6c5bc01a9acd3f
sabin2000pro <sabinlungu293@gmail.com>
2023-02-09 16:20:35 +0000

    Finalising the UI for register page

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -8,6 +8,10 @@ const Navbar = () => {
 
         <nav className = "nav-container">
 
+          <div className = "nav-left">
+              Ether Tix
+          </div>
+
         <ul className = "nav-list-items">
             <li><a className = "nav-item" href="/cart">Cart</a></li>
             <li><a className = "nav-item" href ="/register">Register</a></li>
