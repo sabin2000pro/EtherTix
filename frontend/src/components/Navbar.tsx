39aa648bf0c8da68cbe34a0cf1d22e0fa4933faf
sabin2000pro <sabinlungu293@gmail.com>
2023-02-11 20:52:57 +0000

    Refactoring the search box at the navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -12,15 +12,23 @@ const Navbar = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [searchResults, setSearchResults] = useState<{ name: string, path: string }[]>([]);
 
+  const configureSearchResults = () => {
+    setSearchResults(pages.filter(page => page.name.toLowerCase().includes(searchTerm.toLowerCase() )));
+  }
+
   const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
+
   if (!searchTerm) {
     setSearchResults([]);
-  } else {
-    setSearchResults(
-      pages.filter(page => page.name.toLowerCase().includes(searchTerm.toLowerCase()))
-    );
+  } 
+  
+  else {
+
+      configureSearchResults();
+    
   }
+
 };
 
   return (
@@ -34,21 +42,24 @@ const Navbar = () => {
           </div>
 
           <div className = "nav-centre">
-            <input className = "search-bar" type="text" placeholder="Search" value={searchTerm} onChange={handleSearch}/>
+            <input className = "search-bar" type = "text" placeholder="Search Events" value={searchTerm} onChange={handleSearch}/>
           </div>
 
           <ul className = "nav-list-items">
-            <li><a className = "nav-item" href="/cart">Cart</a></li>
-            <li><a className = "nav-item" href ="/register">Register</a></li>
+             <li><a className = "nav-item" href="/cart">Cart</a></li>
+             <li><a className = "nav-item" href ="/register">Register</a></li>
           </ul>
 
         </nav>
 
         {searchTerm.length > 0 && (
+
           <ul className = "search-results">
             {searchResults.map(result => (
               <li key={result.path}><a className="search-results" href={result.path}>{result.name}</a></li> ))}
           </ul>
+
+
         )}
 
     </header>
