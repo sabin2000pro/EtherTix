4c11d735504a96d6bbc8ad4a90012734bd8b1510
sabin2000pro <sabinlungu293@gmail.com>
2023-02-11 21:15:26 +0000

    Moved the search box into a separate component

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,20 +1,9 @@
 import React, { useState } from 'react';
-
-const pages = [
-  { name: 'Home', path: '/' },
-  { name: 'Cart', path: '/cart' },
-  { name: 'Register', path: '/register' },
-  { name: 'Log In', path: '/login' },
-  { name: 'Update Profile', path: '/update-profile' },
-];
+import Search from './Search';
 
 const Navbar = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [searchResults, setSearchResults] = useState<{ name: string, path: string }[]>([]);
-
-  const configureSearchResults = () => {
-    setSearchResults(pages.filter(page => page.name.toLowerCase().includes(searchTerm.toLowerCase() )));
-  }
 
   const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
@@ -23,12 +12,6 @@ const Navbar = () => {
      setSearchResults([]);
   } 
   
-  else {
-
-      configureSearchResults();
-    
-  }
-
 };
 
   return (
@@ -41,9 +24,7 @@ const Navbar = () => {
             <div className ="nav-header">Ether Tix</div>
           </div>
 
-          <div className = "nav-centre">
-            <input className = "search-bar" type = "text" placeholder="Search Events" value={searchTerm} onChange={handleSearch}/>
-          </div>
+           <Search searchTerm = {searchTerm as any} handleSearch = {handleSearch as any} searchResults = {searchResults as any} />
 
           <ul className = "nav-list-items">
              <li><a className = "nav-item" href="/cart">Cart</a></li>
@@ -51,16 +32,6 @@ const Navbar = () => {
           </ul>
 
         </nav>
-
-        {searchTerm.length > 0 && (
-
-          <ul className = "search-results">
-            {searchResults.map(result => (
-              <li key={result.path}><a className="search-results" href={result.path}>{result.name}</a></li> ))}
-          </ul>
-
-
-        )}
 
     </header>
     )
