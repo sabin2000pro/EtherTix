e7f58530ad936c88d56f4bd6b10262573369b304
sabin2000pro <sabinlungu293@gmail.com>
2023-02-11 11:44:45 +0000

    Implementing functionality for listing an NFT for sale

--- frontend/contracts/TicketNFT.sol
+++ frontend/contracts/TicketNFT.sol
@@ -19,9 +19,10 @@ contract TicketNFT is ERC721URIStorage, Ownable { // NFT Contract for Event Tick
     uint256 public totalTokenSupply; // Total supply for the tokens
 
     mapping (uint256 => NftToken) public tokens; // Create mapping between an Integer and the token struct (1 => Nft data, 2: Nft Data...)
-    mapping (uint256 => address) tokenOwners; // Store the owners of the NFT
+    mapping (uint256 => address) tokenOwner; // Store the owners of the NFT
     mapping (uint256 => bool) public isTokenForSale;
     mapping (uint256 => uint256) public tokensPrice;
+    mapping(string => bool) tokenNames;
     
     constructor() ERC721("Events NFT Ticket", "ENFT") {}
 
@@ -40,7 +41,11 @@ contract TicketNFT is ERC721URIStorage, Ownable { // NFT Contract for Event Tick
     }
 
     function getOwnerOfToken(uint256 _tokenId) public view returns (address) {
-        return tokenOwners[_tokenId];
+        return tokenOwner[_tokenId];
+    }
+
+    function fetchTokenByIndex(uint256 _tokenIndex) public view returns (NftToken memory) {
+        return tokens[_tokenIndex];
     }
 
     function listNftForSale(uint256 _tokenId, uint256 _listingPrice) public { // Function which will list the NFT for sale
