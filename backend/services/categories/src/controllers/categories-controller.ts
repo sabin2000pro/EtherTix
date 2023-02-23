20f2f25ba2cd58d671e40ce23d0cf7a49900f9bf
sabin2000pro <sabinlungu293@gmail.com>
2023-02-23 22:20:12 +0000

    Creating entire new error handler for every service

--- backend/services/categories/src/controllers/categories-controller.ts
+++ backend/services/categories/src/controllers/categories-controller.ts
@@ -1,22 +1,6 @@
-import { BadRequestError, NotFoundError } from '../middleware/error-handler';
 import { StatusCodes } from 'http-status-codes';
 import {Category} from '../models/categories-model';
-import {Request, Response, NextFunction} from 'express'
-declare namespace Express {
-    export interface Request {
-        body: any;
-        params: any;
-        method: any;
-        query: any;
-        files?: Record<any,any>
-    }
-    export interface Response {
-        status: any;
-        json: any;
-        response: any
-    }
-
-  }
+import {Response, NextFunction} from 'express'
 
   // @description: Returns a list of event categories
   // @parameters: request: Request Object, response: Response Object, next: Next Function, user: User Object, statusCode: Status Code of The request
@@ -29,18 +13,18 @@ export const fetchAllCategories = async (request: any, response: any, next: Next
 
         const categories = await Category.find()
         
-        if(!categories) {
-            return next(new NotFoundError("No categories found", StatusCodes.NOT_FOUND))
-        }
+        // if(!categories) {
+        //     return next(new NotFoundError("No categories found", StatusCodes.NOT_FOUND))
+        // }
 
         return response.status(StatusCodes.OK).json({success: true, categories})
     } 
     
     catch(error) {
 
-        if(error) {
-            return next(new BadRequestError(error.message, StatusCodes.BAD_REQUEST));
-        }
+        // if(error) {
+        //     return next(new BadRequestError(error.message, StatusCodes.BAD_REQUEST));
+        // }
 
     }
 
@@ -53,9 +37,9 @@ export const fetchCategoryByID = async (request: any, response: any, next: NextF
       const id = request.params.id;
       const category = await Category.findById(id);
 
-      if(!category) {
-         return next(new NotFoundError("Category with that ID cannot be found", StatusCodes.NOT_FOUND));
-      }
+    //   if(!category) {
+    //      return next(new NotFoundError("Category with that ID cannot be found", StatusCodes.NOT_FOUND));
+    //   }
 
        return response.status(StatusCodes).json({success: true, category});
 
@@ -63,9 +47,9 @@ export const fetchCategoryByID = async (request: any, response: any, next: NextF
     
     catch(error) {
 
-       if(error) {
-            return next(new BadRequestError(error.message, StatusCodes.BAD_REQUEST));
-       }
+    //    if(error) {
+    //         return next(new BadRequestError(error.message, StatusCodes.BAD_REQUEST));
+    //    }
 
     }
 
@@ -79,9 +63,9 @@ export const createNewCategory = async (request: any, response: any, next: NextF
         const event = request.body.event;
         const {name, categoryType, isTrending, isNew} = request.body;
 
-        if(!name || !categoryType || !isTrending || !isNew) {
-            return next(new BadRequestError("One of the categories fields are missing, please check again", StatusCodes.NOT_FOUND));
-        }
+        // if(!name || !categoryType || !isTrending || !isNew) {
+        //     return next(new BadRequestError("One of the categories fields are missing, please check again", StatusCodes.NOT_FOUND));
+        // }
 
         const category = await Category.create({name, categoryType, isTrending, isNew, event});
         await category.save();
@@ -91,9 +75,9 @@ export const createNewCategory = async (request: any, response: any, next: NextF
     
     catch(error) {
 
-        if(error) {
-            return next(new BadRequestError(error.message, StatusCodes.BAD_REQUEST));
-       }
+    //     if(error) {
+    //         return next(new BadRequestError(error.message, StatusCodes.BAD_REQUEST));
+    //    }
 
     }
 
@@ -106,9 +90,9 @@ export const editCategoryByID = async (request: any, response: any, next: NextFu
         const id = request.params.id;
         let category = await Category.findById(id);
 
-        if(!category) {
-            return next(new BadRequestError("Category with that ID cannot be found on the server-side", StatusCodes.BAD_REQUEST));
-        }
+        // if(!category) {
+        //     return next(new BadRequestError("Category with that ID cannot be found on the server-side", StatusCodes.BAD_REQUEST));
+        // }
 
         category = await Category.findByIdAndUpdate(id, request.body, {new: true, runValidators: true});
         await category.save();
@@ -117,9 +101,9 @@ export const editCategoryByID = async (request: any, response: any, next: NextFu
     
     catch(error) {
 
-        if(error) {
-            return next(new BadRequestError(error.message, StatusCodes.BAD_REQUEST));
-       }
+    //     if(error) {
+    //         return next(new BadRequestError(error.message, StatusCodes.BAD_REQUEST));
+    //    }
 
     }
 
@@ -135,9 +119,9 @@ export const deleteCategoryByID = async (request: any, response: any, next: Next
     
     catch(error) {
 
-        if(error) {
-            return next(new BadRequestError(error.message, StatusCodes.BAD_REQUEST));
-       }
+    //     if(error) {
+    //         return next(new BadRequestError(error.message, StatusCodes.BAD_REQUEST));
+    //    }
     }
 
 }
@@ -151,9 +135,9 @@ export const deleteCategories = async (request: any, response: any, next: NextFu
     
     catch(error) {
 
-        if(error) {
-            return next(new BadRequestError(error.message, StatusCodes.BAD_REQUEST));
-       }
+    //     if(error) {
+    //         return next(new BadRequestError(error.message, StatusCodes.BAD_REQUEST));
+    //    }
 
     }
     
@@ -164,18 +148,18 @@ export const fetchTrendingCategories = async (request: any, response: any, next:
 
         const trendingCategories = await Category.find({isTrending: true});
 
-        if(!trendingCategories) {
-            return next(new BadRequestError("No trending categories found", StatusCodes.BAD_REQUEST));
-        }
+        // if(!trendingCategories) {
+        //     return next(new BadRequestError("No trending categories found", StatusCodes.BAD_REQUEST));
+        // }
 
         return response.status(StatusCodes.OK).json({success: true, trendingCategories});
     } 
     
     catch(error) {
         
-        if(error) {
-            return next(new BadRequestError(error.message, StatusCodes.BAD_REQUEST));
-       }
+    //     if(error) {
+    //         return next(new BadRequestError(error.message, StatusCodes.BAD_REQUEST));
+    //    }
 
     }
 
