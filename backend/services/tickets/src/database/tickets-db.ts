1b0da7ccfb50089bc4d6d5cb46c672d4c5046ff5
sabin2000pro <sabinlungu293@gmail.com>
2023-02-23 13:25:24 +0000

    Implementing independent database for the tickets service

--- backend/services/tickets/src/database/tickets-db.ts
+++ backend/services/tickets/src/database/tickets-db.ts
@@ -1,12 +1,18 @@
 import mongoose from "mongoose";
 
+const TICKETS_DB_URI = process.env.TICKETS_DB_URI;
+
 export default () => {
 
 const connectTicketsSchema = async (...args: unknown[]) => {
 
     try {
 
-        return await mongoose.connect("mongodb+srv://sabin2000:123mini123@ethertix.ahxythi.mongodb.net/auth-db?retryWrites=true&w=majority").then(conn => {
+        return await mongoose.connect(TICKETS_DB_URI).then(conn => {
+
+            if(TICKETS_DB_URI === undefined) {
+                return console.log(`The tickets DB URI is undefined`)
+            }
 
             if(conn.connection) {
                 return console.log(`Connected to ticket schema...`)
