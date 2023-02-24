cee174932d8bb4fd955cbc6b5c0e380ffff9ffd5
sabin2000pro <sabinlungu293@gmail.com>
2023-02-24 10:40:50 +0000

    Working on implementing the smart contract functionality for minting, listing and selling NFT

--- frontend/test/TicketNFT.js
+++ frontend/test/TicketNFT.js
@@ -11,15 +11,18 @@ contract("TicketNFT", (accounts) => {
     });
 
     it("should mint a new token", async () => {
-        const name = "Test Token";
+        const name = "Test Mint Token";
         const price = 100;
 
         const receipt = await ticketNFT.mintToken(name, price, { from: accounts[0] });
         const event = receipt.logs.find(log => log.event === "NewTokenMinted");
+
         const newTokenId = event.args._tokenId;
         const token = await ticketNFT.fetchTokenByIndex(newTokenId);
 
         console.log(`Previous Owner : `, token.tokenOwner);
+        console.log(`The event : `, event);
+        console.log(`The receipt `, receipt);
 
         assert.equal(token.tokenOwner, accounts[0]);
         assert.equal(token.tokenName, name);
@@ -27,6 +30,7 @@ contract("TicketNFT", (accounts) => {
     });
 
     it("Should transfer the ownership of the token", async () => {
+        
         const name = "Test Token";
         const price = web3.utils.toWei("0.01", "ether")
        
