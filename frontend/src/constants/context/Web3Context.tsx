d3a0c66eb8440652df4181b4031d2ca2e3fcf13a
sabin2000pro <sabinlungu293@gmail.com>
2023-02-25 12:32:28 +0000

    Working on the Web3 Context file to interact with the compiled smart contract

--- frontend/src/constants/context/Web3Context.tsx
+++ frontend/src/constants/context/Web3Context.tsx
@@ -2,16 +2,24 @@ import React, { useContext, useEffect, useState, createContext} from "react";
 import web3 from "ethers";
 import EventNftContract from '../../contracts/EventMarket.json';
 
+export type IWeb3Context =  {
+    accounts: any;
+    currentBalance: any;
+    currentOwner: any;
+}
+
 export const Web3Context = createContext({} as any); // Create the Context API for Web3
-
-
 
 export const Web3Provider = ({children}: any) => { // Context for Web3
     let DEFAULT_ACCOUNT_BALANCE = 0;
     const [account, setAccount] = useState<string | undefined>("");
     const [balance, setBalance] = useState<number | undefined>(DEFAULT_ACCOUNT_BALANCE);
 
-    return <Web3Context.Provider value = {{}} >
+    const [accountChanged, setAccountChanged] = useState<boolean>(false);
+    const [accountChosen, setAccountChosen] = useState<boolean>(false);
+    const [tokenMinted, setTokenMinted] = useState<boolean>(false);
+
+    return <Web3Context.Provider value = { {} } >
          {children}
     </Web3Context.Provider>
 }
