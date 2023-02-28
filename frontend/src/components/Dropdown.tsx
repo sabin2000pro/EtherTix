989e6c8f392159a231bc03521b63dff0937b8d03
sabin2000pro <sabinlungu293@gmail.com>
2023-02-28 23:22:34 +0000

    Adding drop down menu from bootstrap for creating the user profile

--- frontend/src/components/Dropdown.tsx
+++ frontend/src/components/Dropdown.tsx
@@ -8,11 +8,11 @@ const Dropdown = () => {
 
     <div className ="dropdown profile-dropdown">
 
-        <button className ="btn btn-secondary dropdown-toggle profile-btn" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <button className = "btn dropdown-toggle profile-btn" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
            My Profile
         </button>
 
-      <div className ="dropdown-menu profile-items" aria-labelledby="dropdownMenuButton">
+      <div className = "dropdown-menu profile-items">
             <a className ="dropdown-item" href="#">Settings</a>
             <a className ="dropdown-item" href="#">Logout</a>
       </div>
