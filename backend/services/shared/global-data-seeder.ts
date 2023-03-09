65727d3e215fa650c45562dc9930bcfdde224c77
sabin2000pro <sabinlungu293@gmail.com>
2023-03-09 13:39:59 +0000

    Changing the tickets.json attribute capacity to stock to determine how many tickets are in stock

--- backend/services/shared/global-data-seeder.ts
+++ backend/services/shared/global-data-seeder.ts
@@ -51,8 +51,6 @@ export const loadAllData = async (): Promise<any> => {
 
 export const removeAllData = async (): Promise<any> => {
 
-    let dataRemoved = false;
-
     try {
 
         await User.remove();
@@ -60,9 +58,7 @@ export const removeAllData = async (): Promise<any> => {
         await Ticket.remove();
         await Venue.remove();
 
-        dataRemoved = (!dataRemoved) as boolean;
-
-        console.log(`Data removed from the database.`);
+        console.log(`All the services data removed from the schemas.`);
 
         return process.exit(1);
     } 
