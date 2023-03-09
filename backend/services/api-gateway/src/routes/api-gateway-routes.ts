9b6b3f447b3baf4a017dcc247a023d34924c8eac
sabin2000pro <sabinlungu293@gmail.com>
2023-03-09 13:38:58 +0000

    Changing the tickets.json attribute capacity to stock to determine how many tickets are in stock

--- backend/services/api-gateway/src/routes/api-gateway-routes.ts
+++ backend/services/api-gateway/src/routes/api-gateway-routes.ts
@@ -1,2 +1,8 @@
 import express from 'express';
+import { fetchUserBookedEvents, createNewEvent, editEventDetails} from '../controllers/api-gateway-controllers';
 export const apiGatewayRouter = express.Router();
+
+// API Gateway routes that are going to be forwarded to the Events, Tickets & Venues Microservices
+apiGatewayRouter.route('/my-booked-events/:id').get(fetchUserBookedEvents);
+apiGatewayRouter.route('/edit-event/:id').put(editEventDetails);
+apiGatewayRouter.route('/create-event').post(createNewEvent);
