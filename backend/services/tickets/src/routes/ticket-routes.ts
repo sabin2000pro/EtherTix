477f90a5e427a45ea17223bb14707ce3cd10a720
sabin2000pro <sabinlungu293@gmail.com>
2023-03-09 16:06:13 +0000

    Added code to remove all tickets from the databse

--- backend/services/tickets/src/routes/ticket-routes.ts
+++ backend/services/tickets/src/routes/ticket-routes.ts
@@ -1,7 +1,10 @@
 import express, {Router} from 'express';
-import { fetchAllTickets, createNewTicket, fetchTicketByID, editTicketByID, deleteTicketByID, deleteAllTickets, fetchBasicTickets } from '../controllers/ticket-controller';
+import { fetchAllTickets, createNewTicket, fetchTicketByID, editTicketByID, deleteTicketByID, deleteAllTickets, fetchBasicTickets, fetchStandardTickets, fetchPremiumTickets } from '../controllers/ticket-controller';
 
 export const ticketRouter: Router = express.Router();
 
 ticketRouter.route('/').get(fetchAllTickets as any).post(createNewTicket as any).delete(deleteAllTickets as any)
 ticketRouter.route('/:id').get(fetchTicketByID as any).put(editTicketByID as any).delete(deleteTicketByID as any)
+
+ticketRouter.route('/standard-tickets').get(fetchStandardTickets as any);
+ticketRouter.route('/premium-tickets').get(fetchPremiumTickets as any);
