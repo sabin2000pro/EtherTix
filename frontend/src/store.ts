315c0f9f0aae86154de8b398005b3d05ab150519
sabin2000pro <sabinlungu293@gmail.com>
2023-03-23 12:21:33 +0000

    Implemented the tickets reducer and single ticket reducer to the global store

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,4 +1,4 @@
-import { fetchAllTicketsReducer, fetchSingleTicketByIDReducer } from './reducers/ticket-reducers'
+import { ticketsReducer, singleTicketReducer } from './reducers/ticket-reducers'
 import {configureStore} from '@reduxjs/toolkit'
 import { fetchAllEvents, fetchSingleEventReducer } from 'reducers/event-reducers'
 import { addToCartReducer, removeItemFromCart } from 'reducers/cart-reducers'
