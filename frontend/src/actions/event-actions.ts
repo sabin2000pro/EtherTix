cc4c8d176be4f7b508942171efec4e3c11fdd446
Andrew Crook <andrewcrook16@gmail.com>
2023-03-27 12:37:38 +0100

    event fetching

--- frontend/src/actions/event-actions.ts
+++ frontend/src/actions/event-actions.ts
@@ -1,7 +1,14 @@
+import { FETCH_ALL_EVENTS_REQUEST } from "constants/event-constants"
 
 export const fetchEventList = () => async (dispatch: any) => {
+
      try {
-    
+       dispatch({type: FETCH_ALL_EVENTS_REQUEST});
+
+       const {data} = await axios.get(`http://localhost:5301/api/v1/events`);
+       console.log("Events Data : ", data);
+
+       
      } 
      
      catch(error: any) {
