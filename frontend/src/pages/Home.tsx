a3c3d9174811d57560e9ade4401b9532549a0865
jayC02 <98602035+jayC02@users.noreply.github.com>
2023-03-30 16:13:15 +0100

    cart

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,14 @@
 import React, { useRef } from 'react';
 import { Link } from 'react-router-dom';
 
-const services = [  { id: 1, title: "Service 1", image: "https://source.unsplash.com/random/400x400", description: "Service 1 description" },  { id: 2, title: "Service 2", image: "https://source.unsplash.com/random/400x400", description: "Service 2 description" },  { id: 3, title: "Service 3", image: "https://source.unsplash.com/random/400x400", description: "Service 3 description" },  { id: 4, title: "Service 4", image: "https://source.unsplash.com/random/400x400", description: "Service 4 description" },  { id: 5, title: "Service 5", image: "https://source.unsplash.com/random/400x400", description: "Service 5 description" },  { id: 6, title: "Service 6", image: "https://source.unsplash.com/random/400x400", description: "Service 6 description" },  { id: 7, title: "Service 7", image: "https://source.unsplash.com/random/400x400", description: "Service 7 description" },  { id: 8, title: "Service 8", image: "https://source.unsplash.com/random/400x400", description: "Service 8 description" }];
+const services = [  { id: 1, title: "Service 1", image: "https://source.unsplash.com/random/400x400", description: "Service 1 description" },  
+{ id: 2, title: "Service 2", image: "https://source.unsplash.com/random/400x400", description: "Service 2 description" },  
+{ id: 3, title: "Service 3", image: "https://source.unsplash.com/random/400x400", description: "Service 3 description" },  
+{ id: 4, title: "Service 4", image: "https://source.unsplash.com/random/400x400", description: "Service 4 description" },  
+{ id: 5, title: "Service 5", image: "https://source.unsplash.com/random/400x400", description: "Service 5 description" },  
+{ id: 6, title: "Service 6", image: "https://source.unsplash.com/random/400x400", description: "Service 6 description" },  
+{ id: 7, title: "Service 7", image: "https://source.unsplash.com/random/400x400", description: "Service 7 description" },  
+{ id: 8, title: "Service 8", image: "https://source.unsplash.com/random/400x400", description: "Service 8 description" }];
 
 const Home = () => {
   const containerRef = useRef(null);
@@ -32,6 +39,7 @@ const Home = () => {
         <div className="container text-center" onWheel={handleWheel} ref={containerRef}>
           <h2 className="services-title">Services</h2>
           <div className="services-items">
+
             {services.map((service) => (
               <div key={service.id} className="services-item">
                 <div className="services-image">
