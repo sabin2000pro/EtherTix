943804199acf804c212650b19003ad51f271743a
sabin2000pro <sabinlungu293@gmail.com>
2023-04-21 13:11:44 +0100

    Working on the CRUD operations for the bookings microservice

--- backend/services/booking-service/src/controllers/booking-controllers.ts
+++ backend/services/booking-service/src/controllers/booking-controllers.ts
@@ -1,5 +1,14 @@
+import { NextFunction } from 'express';
+import { Booking } from "../model/booking-model"
 
-export const fetchAllBookings = async (request: any, response: any, next: any): Promise<any> => {
+export const fetchAllBookings = async (request: any, response: any, next: NextFunction): Promise<any> => {
 
 }
 
+export const fetchUserBookings = async (request: any, response: any, next: NextFunction): Promise<any> => {
+
+}
+
+export const fetchSingleBookingByID = async (request: any, response: any, next: NextFunction): Promise<any> => {
+
+}
