fb3cedf6ee65c1c9a505bf7970c4aeaf7ca0de1d
sabin2000pro <sabinlungu293@gmail.com>
2023-04-21 15:32:26 +0100

    Adding the reviews constants in the review-constants.ts file

--- frontend/src/actions/review-actions.ts
+++ frontend/src/actions/review-actions.ts
@@ -1,11 +1,11 @@
 import { Dispatch } from "redux"
 
-export const fetchAllUserReviews = () => async (dispatch: Dispatch) => {
+export const fetchAllUserReviews = () => async (dispatch: Dispatch): Promise<void> => {
 
 }
 
 export const fetchEventReviews = (id: string) => async (dispatch: Dispatch): Promise<void> => {
-    
+
 }
 
 export const fetchReviewByID = (id: string) => async (dispatch: Dispatch): Promise<void> => {
