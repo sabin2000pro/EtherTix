c1a68a9be1e7e9a693f2230cae9589a4715a7823
sabin2000pro <sabinlungu293@gmail.com>
2023-04-21 12:33:46 +0100

    Writing database connection code for the booking schema

--- backend/services/categories/src/database/categories-schema.ts
+++ backend/services/categories/src/database/categories-schema.ts
@@ -1,24 +1,21 @@
 require('dotenv').config();
-import mongoose from "mongoose";
+import mongoose, { Connection } from "mongoose";
 
 const CATEGORIES_SERVICE_DB_URI = process.env.CATEGORIES_SERVICE_DB_URI
 
-export const connectCategoriesDatabase = async (...args: unknown[]) => {
+export const connectCategoriesDatabase = async () => {
 
     try {
 
-        const connection = mongoose.connect(CATEGORIES_SERVICE_DB_URI)
+        const currConnection = await mongoose.connect(CATEGORIES_SERVICE_DB_URI)
 
-            if(conn.connection) {
+            if(currConnection.connection) {
                 return console.log(`Connected to the Categories Service database schema successfully`)
             }
 
             else {
                 return console.log(`Could not connect to categories schema`)
             }
-
-
-       
     } 
     
     catch(error: any) {
