c10d28667d700ef04330d0a23bb0f9da14be58b8
sabin2000pro <sabinlungu293@gmail.com>
2023-04-22 00:14:01 +0100

    Added the tickets service to the build-services command within the makefile

--- frontend/src/actions/booking-actions.ts
+++ frontend/src/actions/booking-actions.ts
@@ -1,17 +1,37 @@
+import { FETCH_BOOKINGS_REQUEST, FETCH_SINGLE_BOOKING_REQUEST, FETCH_BOOKINGS_FAIL } from './../constants/booking-constants';
 import { Dispatch } from 'redux';
+import axios from 'axios';
 
 export const fetchBookings = () => async (dispatch: Dispatch): Promise<void> => {
+    
     try {
-        dispatch({type: FETCH_BOO})
+
+        dispatch({type: FETCH_BOOKINGS_REQUEST});
+        const {data} = await axios.get(`http://localhost:5050/api/v1/bookings`);
+        
+        console.log(data);
     } 
     
-    catch(error) {
+    catch(error: any) {
+
+        if(error) {
+             dispatch({type: FETCH_BOOKINGS_FAIL, payload: error.data.response.message});
+        }
+
 
     }
 }
 
 export const fetchSingleBooking = (id: string) => async (dispatch: Dispatch): Promise<void> => {
+
     try {
+
+        if(!id) {
+            throw new Error(`ID ${id} not found`);
+        }
+
+        dispatch({type: FETCH_SINGLE_BOOKING_REQUEST});
+
 
     } 
     
