c977537daa2b2e9f0b8e1ef32e4af04791f30514
sabin2000pro <sabinlungu293@gmail.com>
2023-04-21 13:58:14 +0100

    Starting up the bookings microservice server on port 5050

--- backend/services/booking-service/src/app.ts
+++ backend/services/booking-service/src/app.ts
@@ -1,5 +1,6 @@
-import { NextFunction } from 'express';
 require('dotenv').config();
+import { StatusCodes } from 'http-status-codes';
+import { NextFunction } from 'express';
 import express from 'express';
 import morgan from 'morgan';
 
@@ -9,7 +10,7 @@ app.use(express.json());
 app.use(morgan('dev'));
 
 app.get('/', (request: any, response: any, next: NextFunction) => {
-
+return response.status(StatusCodes.OK).json({success: true, message: "Bookings Microservice Root Route"})
 })
 
 export {app};
