0cb563850305346240bf451d89f30b002e17af06
sabin2000pro <sabinlungu293@gmail.com>
2023-04-21 23:56:21 +0100

    Loading the bookings JSON data through the global data seeder file

--- backend/services/shared/global-data-seeder.ts
+++ backend/services/shared/global-data-seeder.ts
@@ -3,11 +3,13 @@ import { User } from '../authentication/src/models/user-model'
 import {Event} from  '../events-service/src/models/event-model'
 import {Ticket} from '../tickets/src/models/ticket-model';
 import {Venue} from '../venues/src/models/venue-model'
+import { Booking } from '../booking-service/src/model/booking-model';
 
 import {connectAuthSchema} from  '../authentication/src/database/auth-schema'
 import connectEventsDatabase from '../events-service/src/database/event-schema'
 import {connectTicketsSchema} from '../tickets/src/database/tickets-db'
 import { connectVenuesSchema } from '../venues/src/database/venues-db';
+import { connectBookingSchema } from './../booking-service/src/database/booking-schema';
 
 const users = require( '../authentication/src/data/users.json')
 const events = require('../events-service/src/data/events.json');
