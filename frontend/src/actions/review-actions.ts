97f734016f0882753db1def7369b54cf29a24ea0
sabin2000pro <sabinlungu293@gmail.com>
2023-04-21 15:26:20 +0100

    Refactoring the indentation within the auth controller, adding whitespace and some internal commentary

---
+++ frontend/src/actions/review-actions.ts
@@ -0,0 +1,9 @@
+import { Dispatch } from "redux"
+
+export const fetchAllUserReviews = () => async (dispatch: Dispatch) => {
+
+}
+
+export const fetchReviewByID = (id: string) => async (dispatch: Dispatch) => {
+
+}
