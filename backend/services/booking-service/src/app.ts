7e3f02bddd5f1b82a04544bb04933f3ba650697b
sabin2000pro <sabinlungu293@gmail.com>
2023-04-21 13:57:49 +0100

    Starting up the bookings microservice server on port 5050

--- backend/services/booking-service/src/app.ts
+++ backend/services/booking-service/src/app.ts
@@ -1,3 +1,15 @@
+import { NextFunction } from 'express';
 require('dotenv').config();
 import express from 'express';
 import morgan from 'morgan';
+
+const app: any = express();
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.get('/', (request: any, response: any, next: NextFunction) => {
+
+})
+
+export {app};
