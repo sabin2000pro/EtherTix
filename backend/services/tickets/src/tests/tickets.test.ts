8d309f47228c23d10f303398d3546253945c4837
sabin2000pro <sabinlungu293@gmail.com>
2023-04-24 12:22:15 +0100

    Writing auth reducer code for login success / fail

--- backend/services/tickets/src/tests/tickets.test.ts
+++ backend/services/tickets/src/tests/tickets.test.ts
@@ -3,9 +3,31 @@ import supertest from "supertest";
 import mongoose from 'mongoose';
 import {app} from "../app";
 
+const TICKETS_SERVICE_DB_URI = process.env.TICKETS_SERVICE_DB_URI || ""
+
 beforeAll(async () => {
     // Before starting the unit tests
-    return await mongoose.connect(``);
+    return await mongoose.connect(TICKETS_SERVICE_DB_URI);
+})
+
+describe("Fetch All Tickets - Unit Test Suite", () => {
+
+    it("Fetch All Tickets - Unit Test I", async () => {
+
+    })
+
+})
+
+describe("Fetch Single Ticket - Unit Test Suite", () => {
+    it("Fetch Single Ticket By ID - Valid ID", async () => {
+
+    })
+
+    it("Fetch Single Ticket By ID - Missing ID", async () => {
+
+    })
+
+    
 })
 
 afterAll((done) => {
