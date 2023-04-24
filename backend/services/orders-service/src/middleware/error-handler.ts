0320753528bbd07b353d71f7406438cf9de654d7
sabin2000pro <sabinlungu293@gmail.com>
2023-04-24 21:12:14 +0100

    Reused the order controllers from my previous project

--- backend/services/orders-service/src/middleware/error-handler.ts
+++ backend/services/orders-service/src/middleware/error-handler.ts
@@ -1,1 +1,11 @@
+import { StatusCodes } from 'http-status-codes';
+import { NextFunction } from 'express';
 import { ErrorResponse } from "../utils/error-response";
+
+export const errorHandler = (err: any, request: any, response: any, next: NextFunction): Promise<any> => {
+    let error = {...err}
+    err.message = error.message;
+    err.stack = error.stack
+
+    return response.status(StatusCodes.INTERNAL_SERVER_ERROR).json({success: false, message: error.message, stack: error.stack, error});
+}
