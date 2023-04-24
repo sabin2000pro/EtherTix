88a08c0449feb27bddf9204dab485df414898c35
sabin2000pro <sabinlungu293@gmail.com>
2023-04-24 14:28:16 +0100

    Wrote API to like an event

--- backend/services/events-service/src/routes/event-routes.ts
+++ backend/services/events-service/src/routes/event-routes.ts
@@ -1,7 +1,9 @@
 import express, { Router } from "express";
-import { fetchAllEvents, fetchSingleEvent, createNewEvent, editEventByID, deleteEvents, deleteEventByID } from "../controllers/event-controller";
+import { fetchAllEvents, likeEvent, unlikeEvent, fetchSingleEvent, createNewEvent, editEventByID, deleteEvents, deleteEventByID } from "../controllers/event-controller";
 
 export const eventRouter: Router = express.Router();
 
 eventRouter.route('/').get(fetchAllEvents as any).post(createNewEvent).delete(deleteEvents as any);
-eventRouter.route('/:id').get(fetchSingleEvent as any).put(editEventByID).delete(deleteEventByID as any);
+eventRouter.route('/:eventId').get(fetchSingleEvent as any).put(editEventByID).delete(deleteEventByID as any);
+eventRouter.route('/:eventId/like-event').put(likeEvent as any);
+eventRouter.route('/:eventId/unlike-event').delete(unlikeEvent as any);
