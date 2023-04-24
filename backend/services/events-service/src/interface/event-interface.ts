7f9308d13b80f638d7725b63f728a22022e76796
sabin2000pro <sabinlungu293@gmail.com>
2023-04-24 12:29:59 +0100

    Writing auth reducer code for login success / fail

---
+++ backend/services/events-service/src/interface/event-interface.ts
@@ -0,0 +1,53 @@
+import mongoose from 'mongoose';
+
+export interface EventAttributes { // Interface for the event attributes
+    name: string;
+    summary: string;
+    description: string;
+    startAt: Date;
+    slug: string;
+    createdAt: Date;
+    changedAt: Date;
+    endsAt: Date;    
+    event_url: string;
+    isOnline: boolean
+    format: string;
+    hasSeating: boolean;
+    image: string;
+    capacity: number;
+    salesStatus: string;
+    isSoldOut: boolean;
+    reservedSeating: boolean;
+    eventStatus: string;
+
+    organiser: mongoose.Schema.Types.ObjectId; // Organiser ID (User) of the specific event
+    venue: mongoose.Schema.Types.ObjectId; // Venue ID of the specific Event
+    tickets: mongoose.Schema.Types.ObjectId; // The Ticket ID of the specific Event
+    category: mongoose.Schema.Types.ObjectId; // Category ID of the Specifid Event
+}
+
+export interface EventDocument extends mongoose.Model<EventAttributes> {
+    name: string;
+    summary: string;
+    description: string;
+    startAt: Date;
+    slug: string;
+    endsAt: Date;
+    createdAt: Date;
+    image: string;
+    changedAt: Date;
+    format: string
+    isOnline: boolean;
+    capacity: number;
+    event_url: string;
+    reservedSeating: boolean;
+    isSoldOut: boolean;
+    hasSeating: boolean;
+    salesStatus: string;
+    eventStatus: string;
+
+    organiser: mongoose.Schema.Types.ObjectId; // Event organiser (User ID)
+    venue: mongoose.Schema.Types.ObjectId; // The venue for which an event belongs to
+    tickets: mongoose.Schema.Types.ObjectId; // Ticket corresponding to an event
+    category: mongoose.Schema.Types.ObjectId;
+}
