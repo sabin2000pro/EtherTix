7788b4a25e83812149ae86d7ebfa186cfb2522d8
sabin2000pro <sabinlungu293@gmail.com>
2023-04-25 14:42:42 +0100

    Testing the connection to the orders schema

--- backend/services/orders-service/src/database/order-schema.ts
+++ backend/services/orders-service/src/database/order-schema.ts
@@ -2,15 +2,32 @@ require('dotenv').config();
 import mongoose from 'mongoose';
 
 
+const ORDERS_SERVICE_DB_URI = process.env.ORDERS_SERVICE_DB_URI;
 
 export const connectOrderSchema = async () => {
     try {
+        const currConnection = await mongoose.connect(ORDERS_SERVICE_DB_URI as any);
+
+        if(currConnection.connection) {
+            console.log(`Connected to the orders microservice schema ..`);
+        }
+
+        else {
+            console.log(`Could not connect to the database - closing the connection..`)
+            mongoose.connection.close(); // Otherwise if cannot connect, just close connection to db
+
+            process.exit(1);
+        }
+
 
     } 
     
-    catch(error) {
-
+    catch(error: any) {
+    if(error) {
+        throw new Error(error);
     }
 
+
+    }
 
 }
--- backend/services/shared/global-data-seeder.ts
+++ backend/services/shared/global-data-seeder.ts
@@ -4,12 +4,14 @@ import {Event} from  '../events-service/src/models/event-model'
 import {Ticket} from '../tickets/src/models/ticket-model';
 import {Venue} from '../venues/src/models/venue-model'
 import { Booking } from '../booking-service/src/model/booking-model';
+import {Order} from '../orders-service/src/model/order-model';
 
 import {connectAuthSchema} from  '../authentication/src/database/auth-schema'
 import connectEventsDatabase from '../events-service/src/database/event-schema'
 import {connectTicketsSchema} from '../tickets/src/database/tickets-db'
 import { connectVenuesSchema } from '../venues/src/database/venues-db';
 import { connectBookingSchema } from './../booking-service/src/database/booking-schema';
+
 
 const users = require( '../authentication/src/data/users.json')
 const events = require('../events-service/src/data/events.json');
