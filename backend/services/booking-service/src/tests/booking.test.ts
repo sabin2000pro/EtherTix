6b78c37aea24d043595f20823f5025efc3a1e865
sabin2000pro <sabinlungu293@gmail.com>
2023-04-25 15:22:33 +0100

    preparing to write unit tests for the bookings service

--- backend/services/booking-service/src/tests/booking.test.ts
+++ backend/services/booking-service/src/tests/booking.test.ts
@@ -4,13 +4,23 @@ import request from "supertest"
 import mongoose from "mongoose"
 import {app} from '../app';
 
-
 beforeAll(async() => {
     return await mongoose.connect("");
 })
 
+describe("Fetch All Bookings - API Unit Test Suite", () => {
+
+})
+
+describe("Fetch Booking By ID - API Unit Test Suite", () => {
+
+
+})
+
+describe("Create New ")
+
 afterAll((done) => {
         // Close connection to the database
-        mongoose.connection.close();
+    mongoose.connection.close();
     done();
 })
