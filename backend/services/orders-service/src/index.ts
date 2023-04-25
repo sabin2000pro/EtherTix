7db41210a4e3a9bb927fe0d5f7a3f17ff476ee7f
sabin2000pro <sabinlungu293@gmail.com>
2023-04-25 14:59:34 +0100

    Attempting to connect to the orders service through the docker ocntainer

--- backend/services/orders-service/src/index.ts
+++ backend/services/orders-service/src/index.ts
@@ -1,10 +1,10 @@
 require('dotenv').config();
 import { app } from "./app"
 
-const ORDERS_SERVICE_PORT = process.env.ORDERS_SERVICE_PORT || 5299;
+const ORDERS_SERVICE_PORT = process.env.ORDERS_SERVICE_PORT || 5051;
 
 // Start of authentication server
-export const startAuthServer = async () => {
+export const startOrdersServer = async () => {
 
       return app.listen(ORDERS_SERVICE_PORT, () => {
         console.log(`Orders microservice service listening for requests on port:  ${ORDERS_SERVICE_PORT}`);
@@ -12,4 +12,4 @@ export const startAuthServer = async () => {
 
 }
 
-startAuthServer()
+startOrdersServer();
