ec583bc8d13c92969bfebbdc8538d8f3ab80d2ae
sabin2000pro <sabinlungu293@gmail.com>
2023-04-25 14:46:54 +0100

    Imported the connection to the orders database in the app.ts file

--- backend/services/orders-service/src/tests/orders-service.test.ts
+++ backend/services/orders-service/src/tests/orders-service.test.ts
@@ -3,3 +3,5 @@ import { StatusCodes } from 'http-status-codes';
 import request from "supertest"
 import mongoose from "mongoose"
 import {app} from '../app';
+
+const ORDERS_SERVICE_DB_URI = process.env.ORDERS_SERVICE_DB_URI;
