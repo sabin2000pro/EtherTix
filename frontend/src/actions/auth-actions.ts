7740158ac2ed1998a3fe4dd1df403b221ba8550f
sabin2000pro <sabinlungu293@gmail.com>
2023-04-25 13:07:08 +0100

    Added register account validations for the auth-actions

--- frontend/src/actions/auth-actions.ts
+++ frontend/src/actions/auth-actions.ts
@@ -3,9 +3,42 @@ import { Dispatch } from "redux";
 import axios from 'axios';
 import { AUTH_REGISTER_URI } from "api/auth/auth-uris/auth-uris";
 
+const validateRegisterData = (username: string, email: string, password: string, passwordConfirm: string) => {
+    if (!username) {
+        throw new Error('Username is required');
+    }
+
+    if (!email) {
+        throw new Error('Email is required');
+    }
+
+    if (!password) {
+        throw new Error('Password is required');
+    }
+
+    if (password !== passwordConfirm) {
+        throw new Error('Password and confirm password do not match');
+    }
+
+    if (username.length < 3) {
+        throw new Error('Username must be at least 3 characters long');
+    }
+
+    if (!/^\S+@\S+\.\S+$/.test(email)) {
+        throw new Error('Invalid email format');
+    }
+
+    if (password.length < 8) {
+        throw new Error('Password must be at least 8 characters long');
+    }
+
+}
+
 export const registerUser = (username: string, email: string, password: string, passwordConfirm: string) => async (dispatch: Dispatch): Promise<void> => {
-    
+
     try {
+
+        validateRegisterData(username, email, password, passwordConfirm)
 
        dispatch({type: REGISTER_USER_REQUEST});
 
