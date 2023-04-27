e497837cd092f5a4ad262c358e6902c2a654dfa5
sabin2000pro <sabinlungu293@gmail.com>
2023-04-27 09:38:35 +0100

    Adding search query to the events API

--- backend/services/events-service/src/controllers/event-controller.ts
+++ backend/services/events-service/src/controllers/event-controller.ts
@@ -12,8 +12,7 @@ export const fetchAllEvents = asyncHandler(async (request: any, response: any, n
     const keyword = request.query.searchKeyword ? {name: {$regex: searchKeyword, $options: 'i'}} : {};
     const totalEvents = await Event.countDocuments({...keyword});
     const events = await Event.find({...keyword})
-
-    console.log(`Events : `, events);
+    
     console.log(`Keyword : `, keyword);
 
     if(!events) {
