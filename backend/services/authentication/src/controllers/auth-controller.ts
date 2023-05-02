84a6eec4ea3343b41e7837d52a41cbdcd95ac133
sabin2000pro <sabinlungu293@gmail.com>
2023-05-02 09:48:36 +0100

    Uncommented the send login MFA which sends the login verification code

--- backend/services/authentication/src/controllers/auth-controller.ts
+++ backend/services/authentication/src/controllers/auth-controller.ts
@@ -24,12 +24,12 @@ export const sendResetPasswordTokenStatus = async (request: any, response: any, 
   return response.status(StatusCodes.OK).json({ isValid: true });
 };
 
-export const sendLoginMfa = (email: any, userMfa: any) => {
+export const sendLoginMfa = (user: any, userMfa: any) => {
   const transporter = emailTransporter();
 
   return transporter.sendMail({
     from: "mfa@ethertix.com",
-    to: email,
+    to: user.email,
     subject: "Login MFA Verification",
     html: `
         
